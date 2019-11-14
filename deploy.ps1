31663f29d20bc0ebab03f78f7414d68fdc3bca65
RP <remypottier-imt@mailo.com>
2019-11-14 16:03:26 +0100

    Deploy and configure datacenters of vESXi

--- deploy.ps1
+++ deploy.ps1
@@ -15,15 +15,19 @@ $vcenterUser = $config.vcenter.user
 $vcenterPwd = $config.vcenter.pwd
 # ESXi configuration : IP, root account and password
 $esxConfig = $config.physical_esx
-# List of physical ESX available (ping)
-$pEsx = @()
+# vESXi configuration : root account and password
+$vConfig = $config.virtual_esx
+# List of VM host objects
 $ip2obj = @{ }
-# List of virtualized ESX
-$vEsx = @()
 # ESXi Cluster Name
 $datacenter = "SchoolDatacenter"
-$physicalCluster = "PhysicalESX"
-$vEsxOVF = "./Files/vesx-ovf/vesx-template/vesx-template.ovf"
+$vEsxOVF = "./Files/vesx-ovf/vesx1/vesx1.ovf"
+# Number of vESXi per datacenter
+$nbEsxPerDC = 3
+# Offset for IP address of vESXi
+$ipOffset = 40
+# New Datacenter basename
+$basenameDC = "G"
 
 # Connection to vSphere
 Write-Host "Connecting to vSphere" -ForegroundColor $DefaultColor
@@ -32,82 +36,132 @@ Connect-VIServer -Server $vcenterIp -User $vcenterUser -Password $vcenterPwd
 Write-Host "== Physical ESXi Configuration =="
 # Add ESXi
 Write-Host "Configuring ESXi:" -ForegroundColor $DefaultColor
+$missing = @()
 foreach ($e in $esxConfig) {
     $oReturn = Test-Connection -computername $e.ip -Count 1 -quiet
-    switch ($oReturn) {
-        $true {
-            Write-Host ("+ Add {0} to the ESXi list" -f $e.ip) -ForegroundColor $DefaultColor
-            $pEsx += $e
-        }
-        $false {
-            Write-Host ("- Remove {0} from the ESXi list" -f $e.ip) -ForegroundColor $ErrorColor
-        }
-        Default {
-            Write-Host ("- Remove {0} from the ESXi list" -f $e.ip) -ForegroundColor $ErrorColor
-        }
+    if (!$oReturn) {
+        $missing += $e
     }
 }
-Write-Host $pEsx.Count "ESXi available:" -ForegroundColor $DefaultColor
-$pEsx.foreach{
-    Write-Host $_.ip -ForegroundColor $DefaultColor
+if ($missing.Count -gt 0) {
+    Write-Host $missing.Count "ESXi are missing:" -ForegroundColor $ErrorColor
+    $missing.foreach{
+        Write-Host $_.ip -ForegroundColor $ErrorColor
+    }
+    return
 }
-
-Write-Host "Retrieve the cluster $physicalCluster" -ForegroundColor $DefaultColor
-$dc = Get-Datacenter
-if (!$dc) {
-    Write-Host "Create a new datacenter" -ForegroundColor $DefaultColor
+Write-Host "Retrieve the datacenter $datacenter" -ForegroundColor $DefaultColor
+try {
+    $dc = Get-Datacenter -Name $datacenter
+}
+catch {
+    Write-Host "Creating the datacenter" -ForegroundColor $DefaultColor
     $folder = Get-Folder -NoRecursion
     $dc = New-Datacenter -Location $folder -Name $datacenter
+    Start-Sleep -Seconds 5
 }
-Try {
-    $cl = Get-Cluster -Name $physicalCluster
-}
-Catch {
-    Write-Host "Create the cluster" -ForegroundColor $DefaultColor
-    $cl = New-Cluster -Name $physicalCluster -Location $dc
-}
-foreach ($e in $pEsx) {
+foreach ($e in $esxConfig) {
     try {
-        $ip2obj[$e.ip] = Get-VMHost -Name $e.ip -Location $physicalCluster
+        $ip2obj[$e.ip] = Get-VMHost -Name $e.ip
     }
     catch {
-        Write-Host ("Add the host {0} to $physicalCluster" -f $e.ip) -ForegroundColor $DefaultColor
-        $ip2obj[$e.ip] = Add-VMHost -Name $e.ip -Location $physicalCluster -User $e.user -Password $e.pwd -Force
+        Write-Host ("Add the host {0} to $datacenter" -f $e.ip) -ForegroundColor $DefaultColor
+        $ip2obj[$e.ip] = Add-VMHost -Name $e.ip -Location $datacenter -User $e.user -Password $e.pwd
     }
 }
 Write-Host "== Virtualized ESXi Configuration ==" -ForegroundColor $DefaultColor
 # Create VM from OVF
 $nbNewEsx = 1
-foreach ($e in $pEsx) {
+foreach ($e in $esxConfig) {
     for ($i = 0; $i -lt $e.nb_vesx; $i++) {
+        if ($nbNewEsx -lt 10) {
+            $macAddr = "00:50:56:a1:00:0" + $nbNewEsx
+        }
+        else {
+            $macAddr = "00:50:56:a1:00:" + $nbNewEsx
+        }
         # Create one vESXi
         $NewEsxName = "vesx" + $nbNewEsx
         try {
             $newEsx = Get-VM -Name $NewEsxName
+            $mac = Get-NetworkAdapter -VM $newEsx | Select-Object MacAddress
+            if ($mac.MacAddress -eq $macAddr) {
+                Write-Host ("Checking {0} state" -f $NewEsxName) -ForegroundColor $DefaultColor
+                if ($newEsx.PowerState -eq "PoweredOn") {
+                    Write-Host ("{0} is running" -f $NewEsxName) -ForegroundColor $DefaultColor
+                }
+                else {
+                    Write-Host ("Starting {0}" -f $NewEsxName) -ForegroundColor $DefaultColor
+                    Start-VM -VM $newEsx
+                }
+            }
+            else {
+                Write-Host ("Wrong MAC for {0}" -f $NewEsxName) -ForegroundColor $ErrorColor
+                Write-Host ("Please set MAC address of the VM {0} to {1}" -f $NewEsxName, $macAddr) -ForegroundColor $ErrorColor
+                return
+            }
         }
         catch {
             Write-Host ("Creating the virtualized ESXi " + $NewEsxName) -ForegroundColor $DefaultColor
             $newEsx = Import-vApp -Source $vEsxOVF -VMHost $ip2obj[$e.ip] -Name $NewEsxName -DiskStorageFormat Thin
-            if ($nbNewEsx -lt 10) {
-                $trash = $newEsx | Get-NetworkAdapter | Set-NetworkAdapter -MacAddress ("00:50:56:a1:00:0" + $nbNewEsx) -Confirm:$false -StartConnected:$true
-            }
-            else {
-                $trash = $newEsx | Get-NetworkAdapter | Set-NetworkAdapter -MacAddress ("00:50:56:a1:00:" + $nbNewEsx) -Confirm:$false -StartConnected:$true
-            }
+            $newEsx | Get-NetworkAdapter | Set-NetworkAdapter -MacAddress $macAddr -Confirm:$false -StartConnected:$true | Out-Null
         }
         if ($newEsx.PowerState -eq "PoweredOff") {
             Write-Host ("Power on the VM " + $NewEsxName) -ForegroundColor $DefaultColor
-            $trash = $newEsx | Start-VM
+            $newEsx | Start-VM | Out-Null
         }
         $nbNewEsx++
     }
 }
+$nbNewEsx--
 Write-Host "Waiting the vESX" -ForegroundColor $DefaultColor
-for ($i = 1; $i -lt $nbNewEsx; $i++) {
-    $oReturn = $false
+for ($i = 1; $i -le $nbNewEsx; $i++) {
+    $vesxIP = "192.168.1." + ($ipOffset + $i)
+    $oReturn = Test-Connection -computername $vesxIP -Count 1 -quiet
     while (!$oReturn) {
-        $oReturn = Test-Connection -computername ("192.168.1." + (20 + $i)) -Count 1 -quiet
+        $oReturn = Test-Connection -computername $vesxIP -Count 1 -quiet
         Start-Sleep -Seconds 20
+    }
+}
+
+Write-Host "Create datacenters for the vESXi" -ForegroundColor $DefaultColor
+if ($nbNewEsx % $nbEsxPerDC -eq 0) {
+    $nbDC = $nbNewEsx / $nbEsxPerDC
+}
+else {
+    $nbDC = [math]::Truncate($nbNewEsx / $nbEsxPerDC) + 1
+}
+for ($i = 1; $i -le $nbDC; $i++) {
+    $dcName = $basenameDC + $i
+    try {
+        $dc = Get-Datacenter -Name $dcName
+        Write-Host ("Datacenter {0} already exists" -f $dcName) -ForegroundColor $DefaultColor
+    }
+    catch {
+        Write-Host ("Create a new datacenter" + $dcName) -ForegroundColor $DefaultColor
+        $folder = Get-Folder -NoRecursion
+        $dc = New-Datacenter -Location $folder -Name $dcName
+    }
+}
+Write-Host ("Add vESXi to the datacenter " + $dcName) -ForegroundColor $DefaultColor
+$dcNumber = 0
+for ($i = 1; $i -le $nbNewEsx; $i++) {
+    if (($i - 1) % $nbEsxPerDC -eq 0) {
+        $dc = Get-Datacenter -Name ($basenameDC + ++$dcNumber)
+    }
+    $vesxIP = "192.168.1." + ($ipOffset + $i)
+    try {
+        $ip2obj[$e.ip] = Get-VMHost -Name $vesxIP -Location $dc
+        Write-Host ("vESXi {0} is already connected" -f $vesxIP) -ForegroundColor $DefaultColor
+    }
+    catch {
+        Write-Host ("Add the host {0} to {1}" -f $vesxIP, $dc.Name) -ForegroundColor $DefaultColor
+        $ip2obj[$vesxIP] = Add-VMHost -Name $vesxIP -Location $dc -User $vConfig.user -Password $vConfig.pwd -Force
+        Write-Host ("Removing the datastore of {0}" -f $vesxIP) -ForegroundColor $DefaultColor
+        $ds = Get-Datastore -Id $ip2obj[$vesxIP].DatastoreIdList[0]
+        Remove-Datastore -VMHost $ip2obj[$vesxIP] -Datastore $ds -Confirm:$false
+        Write-Host ("Creating a new datastore for {0}" -f $vesxIP) -ForegroundColor $DefaultColor
+        $ds = New-Datastore -VMHost $ip2obj[$vesxIP] -Name ("vDatastore" + $i) -Path mpx.vmhba0:C0:T0:L0 -Vmfs -Confirm:$false
     }
 }
 
