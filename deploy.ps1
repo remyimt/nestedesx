ea52f46821b2768c58cb300c902a98b954b23c37
RP <remypottier-imt@mailo.com>
2019-11-14 16:37:01 +0100

    remove commented code from the main script

--- deploy.ps1
+++ deploy.ps1
@@ -148,69 +148,9 @@ for ($i = 1; $i -le $nbNewEsx; $i++) {
         Write-Host ("Add the host {0} to {1}" -f $vesxIP, $dc.Name) -ForegroundColor $DefaultColor
         $ip2obj[$vesxIP] = Add-VMHost -Name $vesxIP -Location $dc -User $vConfig.user -Password $vConfig.pwd -Force
         Write-Host ("Removing the datastore of {0}" -f $vesxIP) -ForegroundColor $DefaultColor
-        $ds = Get-Datastore -Id $ip2obj[$vesxIP].DatastoreIdList[0]
+        $ds = Get-Datastore -VMHost $ip2obj[$vesxIP]
         Remove-Datastore -VMHost $ip2obj[$vesxIP] -Datastore $ds -Confirm:$false
         Write-Host ("Creating a new datastore for {0}" -f $vesxIP) -ForegroundColor $DefaultColor
         $ds = New-Datastore -VMHost $ip2obj[$vesxIP] -Name ("vDatastore" + $i) -Path mpx.vmhba0:C0:T0:L0 -Vmfs -Confirm:$false
     }
 }
-
-<#
-# Copy ISO files on datastores
-Write-Host ("{0} physical ESXi and {1} as vCenter host" -f $pEsx.Count, $vcenterHost) -ForegroundColor $DefaultColor
-$st = Get-Datastore
-# DatastoreId = full_path - Use the full path to mount the ISO file
-$id2path = @{ } 
-$firstSt, $otherSt = $st
-Write-Host ("Looking for {0} on datastore {1}" -f $esxInstallerPattern, $firstSt.Name) -ForegroundColor $DefaultColor
-$myFile = Get-ChildItem -Path $firstSt.DatastoreBrowserPath | Where-Object { $_.Name -like $esxInstallerPattern }
-if ($myFile.Count -eq 0) {
-    Write-Host "Upload the file on" $firstSt.Name -ForegroundColor $DefaultColor
-    Copy-DatastoreItem -Item $fileFolder/$esxInstallerPattern -Destination $firstSt.DatastoreBrowserPath
-    # Get the file for later copy
-    $myFile = Get-ChildItem -Path $firstSt.DatastoreBrowserPath | Where-Object { $_.Name -like $esxInstallerPattern }
-}
-else {
-    Write-Host ("{0} exists on {1}" -f $esxInstallerPattern, $firstSt.Name) -ForegroundColor $DefaultColor
-}
-$copyFile = $myFile[0]
-$id2path[$copyFile.DatastoreId] = $copyFile.DatastoreFullPath
-
-foreach ($o in $otherSt) {
-    Write-Host ("Looking for {0} on datastore {1}" -f $esxInstallerPattern, $o.Name) -ForegroundColor $DefaultColor
-    $myFile = Get-ChildItem -Path $o.DatastoreBrowserPath | Where-Object { $_.Name -like $esxInstallerPattern }
-    if ($myFile.Count -eq 0) {
-        Write-Host ("Upload the file on {0} from {1}" -f $o.Name, $firstSt.Name) -ForegroundColor $DefaultColor
-        Copy-DatastoreItem -Item $copyFile -Destination $o.DatastoreBrowserPath
-        $myFile = Get-ChildItem -Path $o.DatastoreBrowserPath | Where-Object { $_.Name -like $esxInstallerPattern }
-    }
-    else {
-        Write-Host ("{0} exists on {1}" -f $esxInstallerPattern, $o.Name) -ForegroundColor $DefaultColor
-    }
-    $id2path[$myFile[0].DatastoreId] = $myFile[0].DatastoreFullPath
-}
-
-### Function Definitions
-Function uploadFile {
-    param (
-        [Parameter(Mandatory)]
-        [string]
-        $filename, 
-        [Parameter(Mandatory)]
-        [VMware.VimAutomation.ViCore.Types.V1.DatastoreManagement.Datastore]
-        $datastore
-    )
-    Write-Host ("Looking for {0} on datastore {1}" -f $filename, $datastore.Name) -ForegroundColor $DefaultColor
-    $myFile = Get-ChildItem -Path $datastore.DatastoreBrowserPath | Where-Object { $_.Name -like $filename }
-    if ($myFile.Count -eq 0) {
-        Write-Host "Upload the file on" $datastore.Name -ForegroundColor $DefaultColor
-        $copyFile = Copy-DatastoreItem -Item $fileFolder/$filename -Destination $datastore.DatastoreBrowserPath
-    }
-    else {
-        Write-Host ("{0} exists on {1}") -f $filename, $datastore -ForegroundColor $DefaultColor
-        $copyFile = $myFile[0]
-    }
-    $copyFile
-}
-### End of functions
-#>
