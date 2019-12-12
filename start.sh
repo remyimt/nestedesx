89feba5705c0214c24c592ffb9fb0b8eca0e2986
RP <remypottier-imt@mailo.com>
2019-12-12 14:57:21 +0100

    Add questions for asking the password

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 host=$(grep "vcenter" -A 5 configuration.json | grep host | sed  's:.*"\(.*\)",:\1:')
 vcenter=$(grep "vcenter" -A 5 configuration.json | grep ip | sed  's:.*"\(.*\)",:\1:')
+numberRegEx="^[0-9]+$"
 
 echo "Waiting ESXi $host"
 ping -c 1 -W 3 $host
@@ -12,8 +13,14 @@ while [ $result -ne 0 ]; do
 done
 sleep 20
 echo "Starting vCenter $vcenter"
+echo "Enter the password for $host"
 id=$(ssh -o StrictHostKeyChecking=no root@$host "vim-cmd vmsvc/getallvms" | grep Embedded | cut -d ' ' -f1)
+if ! [[ $id =~ $numberRegEx ]]; then
+  echo "Failed to retrieve the ID of the VM with the vCenter"
+  exit 13
+fi
 echo "Starting the VM with the id $id"
+echo "Enter the password for $host"
 ssh -o StrictHostKeyChecking=no root@$host "vim-cmd vmsvc/power.on $id"
 echo "Waiting vCenter $vcenter"
 ping -c 1 -W 3 $vcenter
