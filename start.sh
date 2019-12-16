fe292e2d6f8e7c377c388961067e29e6948e4b48
RP <remypottier-imt@mailo.com>
2019-12-16 11:02:15 +0100

    FIX wait for the vSphere web server; usage modification

--- start.sh
+++ start.sh
@@ -4,14 +4,29 @@ vcenter=$(grep "vcenter" -A 5 configuration.json | grep ip | sed  's:.*"\(.*\)",
 numberRegEx="^[0-9]+$"
 HTML_FILE="vcenter.html"
 
-echo "Waiting ESXi $host"
-ping -c 1 -W 3 $host
+if [ "$1" = "web" ]; then
+  echo "Wait for the vSphere Web Server before starting the deployment"
+else
+  if [ "$1" = "quick" ]; then
+    echo "Start the deployment as soon as possible"
+  else
+    echo "Usage: ./start.sh web|quick"
+    echo "  ./start.sh web: Wait for the vSphere Web Server before starting the deployment"
+    echo "  ./start.sh quick: Start the deployment as soon as possible"
+    exit 13
+  fi
+fi
+
+echo "Waiting for ESXi $host"
+ping -c 1 -W 3 $host &> /dev/null
 result=$?
 while [ $result -ne 0 ]; do
   sleep 10
-  ping -c 1 -W 3 $host
+  echo "Waiting for ESXi $host"
+  ping -c 1 -W 3 $host &> /dev/null
   result=$?
 done
+echo "Waiting for the SSH Server"
 sleep 20
 echo "Starting vCenter $vcenter"
 echo "Enter the password for $host"
@@ -23,24 +38,29 @@ fi
 echo "Starting the VM with the id $id"
 echo "Enter the password for $host"
 ssh -o StrictHostKeyChecking=no root@$host "vim-cmd vmsvc/power.on $id"
-echo "Waiting vCenter $vcenter"
-ping -c 1 -W 3 $vcenter
+echo "Waiting for the vCenter $vcenter"
+ping -c 1 -W 3 $vcenter &> /dev/null
 result=$?
 while [ $result -ne 0 ]; do
   sleep 10
-  ping -c 1 -W 3 $vcenter
+  echo "Waiting for the vCenter $vcenter"
+  ping -c 1 -W 3 $vcenter &> /dev/null
   result=$?
 done
 echo "Waiting for the vSphere web service"
-wget https://$vcenter/ui/ --no-check-certificate -O $HTML_FILE &> /dev/null
-res=$(grep "initializing" $HTML_FILE)
-while [ ! -z "$res" ]; do
-  echo "Waiting for the web service"
-  sleep 30
+touch $HTML_FILE
+while [ $(cat $HTML_FILE | wc -l) -eq 0 ]; do
+  sleep 20
+  echo "No response from the vSphere web service"
   wget https://$vcenter/ui/ --no-check-certificate -O $HTML_FILE &> /dev/null
-  res=$(grep "initializing" $HTML_FILE)
+done
+result=$(grep "initializing" $HTML_FILE)
+while [ ! -z "$result" ]; do
+  sleep 30
+  echo "Waiting for the vSphere web service"
+  wget https://$vcenter/ui/ --no-check-certificate -O $HTML_FILE &> /dev/null
+  result=$(grep "initializing" $HTML_FILE)
 done
 rm $HTML_FILE
-
 echo "Starting the NUC cluster"
 pwsh -File ./deploy.ps1
