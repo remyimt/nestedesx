a5b0198a334199caec766867d2d341a20fec7ff1
RP <remypottier-imt@mailo.com>
2020-01-13 11:18:49 +0100

    Add a prompt when asking the password

--- start.sh
+++ start.sh
@@ -38,14 +38,16 @@ echo "The SSH server on $host is running!"
 echo "NOTE: You can copy your SSH keys with './copy-ssh-key.sh $host"
 
 echo "Starting vCenter $vcenter"
-echo "Enter the password ($host_pwd) for $host"
+echo "Enter the password ($host_pwd) for $host / Get the VM ID"
+echo "$ "
 id=$(ssh -o StrictHostKeyChecking=no root@$host "vim-cmd vmsvc/getallvms" | grep Embedded | cut -d ' ' -f1)
 if ! [[ $id =~ $numberRegEx ]]; then
   echo "Failed to retrieve the ID of the VM with the vCenter"
   exit 13
 fi
 echo "Starting the VM with the id $id"
-echo "Enter the password ($host_pwd) for $host"
+echo "Enter the password ($host_pwd) for $host / Power on the VM"
+echo "$ "
 ssh -o StrictHostKeyChecking=no root@$host "vim-cmd vmsvc/power.on $id"
 
 echo "Waiting for the vCenter $vcenter"
--- stop.sh
+++ stop.sh
@@ -7,14 +7,16 @@ numberRegEx="^[0-9]+$"
 pwsh -File ./shutdown-vms.ps1
 
 echo "Stopping the vCenter $vcenter"
-echo "Enter the password ($host_pwd) for $host"
+echo "Enter the password ($host_pwd) for $host / Get the VM ID"
+echo "$ "
 id=$(ssh -o StrictHostKeyChecking=no root@$host "vim-cmd vmsvc/getallvms" | grep Embedded | cut -d ' ' -f1)
 if ! [[ $id =~ $numberRegEx ]]; then
   echo "Failed to retrieve the ID of the VM with the vCenter"
   exit 13
 fi
 echo "Stopping the VM with the id $id"
-echo "Enter the password ($host_pwd) for $host"
+echo "Enter the password ($host_pwd) for $host / Power off the VM"
+echo "$ "
 ssh -o StrictHostKeyChecking=no root@$host "vim-cmd vmsvc/power.off $id"
 ping -c 1 -W 3 $vcenter
 result=$?
@@ -25,5 +27,6 @@ while [ $result -eq 0 ]; do
 done
 
 echo "Power off $host"
-echo "Enter the password ($host_pwd) for $host"
+echo "Enter the password ($host_pwd) for $host / Power off the ESXi"
+echo "$ "
 ssh -o StrictHostKeyChecking=no root@$host "poweroff"
